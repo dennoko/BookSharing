160da018efcf5f2b07f6ad192fe4cf49196783f7
でんのこ <dennnoko314@gmail.com>
2023-12-31 15:36:28 +0900

    UIを調整

--- app/src/main/java/com/example/booksharing/ui_components/NavigationBar.kt
+++ app/src/main/java/com/example/booksharing/ui_components/NavigationBar.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,9 +31,18 @@ fun NavigationBar(navController: NavController) {
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        NavigateButton(navController, route = "home", txt = "Home", modifier = Modifier.weight(1f).fillMaxHeight().padding(horizontal = 4.dp))
-        NavigateButton(navController, route = "myPage", txt = "MyPage", modifier = Modifier.weight(1f).fillMaxHeight().padding(horizontal = 4.dp))
-        NavigateButton(navController, route = "setting", txt = "Setting", modifier = Modifier.weight(1f).fillMaxHeight().padding(horizontal = 4.dp))
+        NavigateButton(navController, route = "home", txt = "Home", modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .padding(horizontal = 4.dp))
+        NavigateButton(navController, route = "myPage", txt = "MyPage", modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .padding(horizontal = 4.dp))
+        NavigateButton(navController, route = "setting", txt = "Setting", modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .padding(horizontal = 4.dp))
     }
 }
 
@@ -40,13 +51,20 @@ fun NavigateButton(navController: NavController, route: String, txt: String, mod
     Button(
         onClick = { navController.navigate(route) },
         shape = RoundedCornerShape(0.dp),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 15.dp,
+        ),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+        ),
         modifier = modifier
     ) {
         Text(
             text = txt,
             fontSize = 20.sp,
             fontWeight = FontWeight.SemiBold,
-            color = MaterialTheme.colorScheme.onPrimary
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
             )
     }
 }
