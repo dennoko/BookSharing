c7e3a41f0d6bc127e91b21138a9feb30687d01a7
SuzukiRyotaro <suzuki879455@gmail.com>
2024-01-01 23:43:23 +0900

    UI作成途中

--- app/src/main/java/com/example/booksharing/ui_components/bookinfoUI.kt
+++ app/src/main/java/com/example/booksharing/ui_components/bookinfoUI.kt
@@ -1,6 +1,48 @@
 package com.example.booksharing.ui_components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.booksharing.firestore.detailforapi
 
 @Composable
-fun bookdetaildisplay(){}
+fun bookdetaildisplay(detailforapi: detailforapi){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = detailforapi.item.volumeInfo.title,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.padding(32.dp))
+            AsyncImage(model =detailforapi.item.volumeInfo.imageLinks
+                , contentDescription = "apiで取ってきた本の画像")
+        }
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(text = "著者：　${detailforapi.detail.owner}")
+            Text(text = "出版社：　${detailforapi.item.volumeInfo.publisher}")
+            Text(text = "出版年：　${detailforapi.item.volumeInfo.publishedDate}")
+            Text(text = "カテゴリ：　${detailforapi.item.volumeInfo.categories}")
+            Text(text = "解説：　${detailforapi.item.volumeInfo.description}")
+            Spacer(modifier = Modifier.width(32.dp))
+            Button(onClick = {//ここでdeletebookを呼び出したい
+            },modifier=Modifier.align(Alignment.End)) {
+                Text(text = "この本を削除する")
+            }
+        }
+    }
+}
